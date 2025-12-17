256ebee5a44aa0bd57ee236edc4fe097a316341e
Yaniv Norman <yanivnorman@microsoft.com>
2025-12-17 12:35:05 +0200

    Update database path configuration for Azure and change OpenAPI server URL to Azure Function

--- config.py
+++ config.py
@@ -2,9 +2,16 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Use /tmp directory on Azure for writable SQLite database
+# Note: /tmp is ephemeral and data will be lost on app restart
+if os.environ.get('WEBSITE_INSTANCE_ID'):  # Running on Azure
+    db_path = '/tmp/petclinic.db'
+else:
+    db_path = os.path.join(basedir, 'petclinic.db')
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'petclinic.db')
+        'sqlite:///' + db_path
     SQLALCHEMY_TRACK_MODIFICATIONS = False
